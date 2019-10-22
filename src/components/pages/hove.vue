d2fb73a48ccc83329a9a8f0a35555d7e6f244378
mmh <1245718734@qq.com>
2019-10-22 08:28:20 +0800

    [v1.0.0]add:语法

--- src/components/pages/hove.vue
+++ src/components/pages/hove.vue
@@ -10,6 +10,19 @@
     <button @click="onclickMarkMa">点击事件</button>
     <h5>{{ markMa }} // v-model:绑定</h5>
     <input type="text" v-model="markMa">
+    <p>{{ navHtml }} // 模版语法</p>
+    <p v-html="navHtml"></p>
+    <a :href="url">绑定跳转连接</a>
+    <div @click="click1">aaaaaa
+      <div @click.stop="click2">qqqq</div>
+    </div>
+    <p v-if="icon">v-if显示隐藏</p>
+    <hr>
+    <div  class="colorRed"
+          :class="{active: tiveNav, grreru: grreruNav}">
+      伪类元素：active
+    </div>
+
   </div>
 </template>
 
@@ -23,14 +36,25 @@ export default {
         'moum',
         'xiaoming'
       ],
-      message: '页面加载于 ' + new Date().toLocaleString(),
-      vif: true,
-      markMa: 'Hello vue.js!'
+      message: '页面加载于 ' + new Date().toLocaleString(), // v-bind
+      vif: true, // v-if
+      markMa: 'Hello vue.js!', // 事件绑定 @click,v-model
+      navHtml: `<h1 style="color:red">HTML模版语法</h1>`, // v-html 模版语法
+      url: 'http://baidu.com', // 绑定url
+      icon: true, // v-if
+      tiveNav: true, // 伪类元素
+      grreruNav: true // 伪类元素
     }
   },
   methods: {
     onclickMarkMa () {
       this.markMa = this.markMa.split('').reverse().join('')
+    },
+    click1() {
+      console.log('click...1')
+    },
+    click2() {
+      console.log('click....2')
     }
   }
 }
@@ -46,4 +70,12 @@ export default {
     border-radius: 2px;
     list-style-type: none;
   }
+  .colorRed {
+    width: 400px;
+    height: 100px;
+    font-size: 19px;
+    border: 2px solid red;
+  }
+  .active {background: yellow;}
+  .grreru {color: green}
 </style>
